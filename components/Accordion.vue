4eed0b2a42b59ac77a96b5cc460d6c032747807b
James Towers <james@songdrop.com>
2017-09-14 14:58:51 +0100

    Add accordion component

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -1,7 +1,9 @@
 <template>
-  <div @click="isOpen = !isOpen" class="accordion" :class="{'open': isOpen}">
-    <h1 class="accordion-handle border-bottom py2 px3">{{ title }}</h1>
-    <slot></slot>
+  <div @click="toggle" class="accordion" :class="{'open': isOpen}" :style="{ height: accordionHeight + 'px' }">
+    <h1 ref="handle" class="accordion-handle border-bottom py2 px3">{{ title }}</h1>
+    <div ref="content" class="accordion-content">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -20,17 +22,26 @@
 
     data () {
       return {
-        isOpen: this.default
+        isOpen: this.default,
+        handleHeight: 60,
+        contentHeight: 0
+      }
+    },
+
+    mounted () {
+      this.handleHeight = this.$refs.handle.clientHeight
+      this.contentHeight = this.$refs.content.clientHeight
+    },
+
+    computed: {
+      accordionHeight () {
+        return this.isOpen ? this.handleHeight + this.contentHeight : this.handleHeight
       }
     },
 
     methods: {
       toggle () {
-        if (this.isOpen) {
-          console.log('close')
-        } else {
-          console.log('open')
-        }
+        this.isOpen = !this.isOpen
       }
     }
   }
@@ -39,6 +50,15 @@
 <style lang="scss">
   @import "~assets/scss/global/variables";
 
+  .accordion{
+    overflow: hidden;
+    transition: height 0.2s $ease-in-out;
+    &.open{
+      .accordion-handle::after{
+        transform: rotate(180deg)
+      }
+    }
+  }
   .accordion-handle{
     cursor: pointer;
     position: relative;
@@ -51,6 +71,7 @@
       border-width: 5px 5px 0;
       border-color: $grey5 transparent transparent;
       position: absolute;
+      transition: transform 0.2s $ease-out-circle;
       content: "";
     }
   }
