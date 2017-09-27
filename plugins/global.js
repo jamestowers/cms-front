02464b651617ec474d55c12025c9a85f6bde9372
James Towers <james@songdrop.com>
2017-09-27 16:01:28 +0200

    Add VeeValidate package

--- plugins/global.js
+++ plugins/global.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VeeValidate from 'vee-validate'
 import PageTitle from '../components/PageTitle.vue'
 import FilesUpload from '../components/fields/Files.vue'
 import flatPickr from 'vue-flatpickr-component'
@@ -6,4 +7,6 @@ import 'flatpickr/dist/flatpickr.css'
 
 Vue.component('page-title', PageTitle)
 Vue.component('files-upload', FilesUpload)
+
+Vue.use(VeeValidate)
 Vue.use(flatPickr)
